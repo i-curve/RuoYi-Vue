42e6550df3763e278f35c569b746e3ec22eca561
i-curve <i-curve@qq.com>
2024-09-22 22:01:44 +0800

    feat: upgrade mybatis-plus

--- ruoyi-common/src/main/java/com/ruoyi/common/core/domain/BaseEntity.java
+++ ruoyi-common/src/main/java/com/ruoyi/common/core/domain/BaseEntity.java
@@ -4,13 +4,15 @@ import java.io.Serializable;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 /**
  * Entity基类
- * 
+ *
  * @author ruoyi
  */
 public class BaseEntity implements Serializable
